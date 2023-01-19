ef5f297912d764a7819588198349bc1f26f442b5
Hariom Mishra <121715030+HariomMishra95@users.noreply.github.com>
2023-01-20 00:47:08 +0530

    Add files via upload

---
+++ build a countdown/script.js
@@ -0,0 +1,22 @@
+const endDate = "29 january 2023 2:17 PM";
+document.getElementById("end-date").innerText = endDate;
+const inputs = document.querySelectorAll("input");
+
+function clock() {
+    const end = new Date(endDate);
+    const now = new Date();
+    const diff = (end - now) / 1000;
+    if (diff < 0) {
+        return;
+    }
+    inputs[0].value = Math.floor(diff / 3600 / 24);
+    inputs[1].value = Math.floor(diff / 3600) % 24;
+    inputs[2].value = Math.floor(diff / 60) % 60;
+    inputs[3].value = Math.floor(diff) % 60;
+}
+clock()
+setInterval(() => {
+    clock()
+}, 1000);
+
+
